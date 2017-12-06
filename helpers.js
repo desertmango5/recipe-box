3a02637c8350d66d7bd60d6c5df7310a9cc1137c
Michael Manges <michael.manges@gmail.com>
2017-12-06 10:24:41 +0400

    Add category sorting buttons

--- helpers.js
+++ helpers.js
@@ -5,3 +5,6 @@ exports.dump = obj => JSON.stringify(obj, null, 2);
 
 // site details
 exports.siteName = 'Recipe Box';
+
+// main food categories
+exports.categories = ['Appetizer', 'Side', 'Main Dish', 'Dessert', 'Gluten-free', 'Keto'];
